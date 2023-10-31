9ca602a39798e0a86f3f5e493cb3a15bbbb2f1fd
liliang <orangecardgame@163.com>
2023-10-31 11:54:27 +0800

    提交

---
+++ src/ABPvNextOrangeAdmin.Domain/System/User/IPostRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ABPvNextOrangeAdmin.System.Organization;
+using Volo.Abp.Domain.Repositories;
+
+namespace ABPvNextOrangeAdmin.System.User;
+
+public interface IPostRepository: IBasicRepository<SysPost>
+{
+   Task<List<long>> GetPostsByUserId(Guid userId);
+   Task<List<long>> GetPostsById(long  postId);
+}
